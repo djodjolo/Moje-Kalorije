0d78156fdb0475dcad0e7ce05fe16761778cc121
djodjolo <djodjolos@gmail.com>
2020-09-09 13:03:22 +0200

    uradjen layaout za unos i layout za listu

--- app/src/main/java/com/ais/mojekalorije/ScrollingActivity.java
+++ app/src/main/java/com/ais/mojekalorije/ScrollingActivity.java
@@ -1,5 +1,7 @@
 package com.ais.mojekalorije;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,11 +10,13 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.InputType;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -30,18 +34,60 @@ public class ScrollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scrolling);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        //ADD BUTTON CLICK
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(ScrollingActivity.this);
+                alert.setTitle("Unesite  sta ste jeli");
+                final EditText input = new EditText(ScrollingActivity.this);
+                alert.setView(input);
+
+                alert.setPositiveButton("Next", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String value2 = input.getText().toString();
+                        AlertDialog.Builder alert2 = new AlertDialog.Builder(ScrollingActivity.this);
+                        alert2.setTitle("Unesite  broj kalorija");
+                        final EditText input = new EditText(ScrollingActivity.this);
+                        alert2.setView(input);
+
+                        alert2.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                String value2 = input.getText().toString();
+                                return;
+
+                            }
+                        });
+                        alert2.setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        return;
+                                    }
+                                });
+
+                        alert2.show();
+                    }
+                });
+                alert.setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                return;
+                            }
+                        });
+
+                alert.show();
             }
         });
 
         String date_n = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date());
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(date_n);
+
+
+
 
         ListView lista = (ListView) findViewById(R.id.listView);
         ArrayList<String> arrayLista = new ArrayList<>();
@@ -50,6 +96,18 @@ public class ScrollingActivity extends AppCompatActivity {
         ArrayAdapter<String> adapterlistaObavestenja = new ArrayAdapter<String>(this,R.layout.row,arrayLista);
         lista.setAdapter(adapterlistaObavestenja);
 
+
+        ListView lista2 = (ListView) findViewById(R.id.listViewHrana);
+        ArrayList<String> arrayLista2 = new ArrayList<>();
+        arrayLista2.add("Ovsene");
+        arrayLista2.add("Punjena");
+        ArrayAdapter<String> adapterlistaObavestenja2 = new ArrayAdapter<String>(this,R.layout.row,arrayLista2);
+        lista2.setAdapter(adapterlistaObavestenja2);
+
+//        String[] countryArray = {"India", "Pakistan", "USA", "UK"};
+//        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.row,R.id.textView, countryArray);
+//        ListView listView = (ListView) findViewById(R.id.listView);
+//        listView.setAdapter(adapter);
 
 
         textView.setOnClickListener(new View.OnClickListener() {
